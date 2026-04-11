009972db7c9c1f609e64b2df08743d544b2a47db
syedAbrar18 <syedabrarsyed24@gmail.com>
2026-04-11 20:35:55 +0530

    Add cart functionality and buyNow feature

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+let cart = 0;
+
+function addCart(item) {
+    cart++;
+    document.getElementById("cartCount").innerText = cart;
+    alert(item + " added to cart");
+}
+
+function buyNow(item) {
+    window.open("https://wa.me/919491874711?text=I want to buy " + item);
+}
