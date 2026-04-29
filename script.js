fee6c9237832f8af404d039801c3e9a0d6e87ca2
syedAbrar18 <syedabrarsyed24@gmail.com>
2026-04-29 19:34:37 +0530

    Change cart from number to array and update functions
    Refactor cart implementation to use an array and update functions accordingly.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let cart = 0;
+let cart = [];
 
-function addCart(item) {
-    cart++;
-    document.getElementById("cartCount").innerText = cart;
-    alert(item + " added to cart");
+function addCart(item){
+    cart.push(item);
+    alert(item + " added to cart 🛒");
 }
 
-function buyNow(item) {
-    window.open("https://wa.me/919491874711?text=I want to buy " + item);
+function orderNow(){
+    if(cart.length === 0){
+        alert("Cart is empty!");
+        return;
+    }
+
+    let msg = "Order Details:%0A";
+
+    cart.forEach((item, index) => {
+        msg += (index + 1) + ". " + item + "%0A";
+    });
+
+    window.open("https://wa.me/919493376611?text=" + msg);
 }
